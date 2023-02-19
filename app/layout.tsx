87a170676d1a53f55254a941f91e9f5513ad2885
ryanbakkerNZ <ryan@ryanbakker.nz>
2023-02-19 15:06:03 +1300

    New chat page and side bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import SideBar from "../components/SideBar";
 import "../styles/globals.css";
 
 export default function RootLayout({
@@ -8,7 +9,17 @@ export default function RootLayout({
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        <div className="flex">
+          <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
+            <SideBar />
+          </div>
+
+          {/* ClientProvider */}
+
+          <div className="bg-[#343541] flex-1">{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,69 @@
+import {
+  BoltIcon,
+  ExclamationTriangleIcon,
+  SunIcon,
+} from "@heroicons/react/24/outline";
+
 function HomePage() {
   return (
-    <div>
-      <h1>ChatGPT Messenger</h1>
+    <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
+      <h1 className="text-5xl font-bold mb-20">ChatGPT</h1>
+
+      <div className="flex space-x-2 text-center">
+        {/* Examples */}
+        <div>
+          <div className="flex flex-col items-center justify-center mb-5">
+            <SunIcon className="h-8 w-8" />
+            <h2>Examples</h2>
+          </div>
+
+          <div className="space-y-2">
+            <p className="infoText">"Explain something to me"</p>
+            <p className="infoText">
+              "What is the difference between a dog and a cat?"
+            </p>
+            <p className="infoText">"What is the color of the sun?"</p>
+          </div>
+        </div>
+
+        {/* Capabilities */}
+        <div>
+          <div className="flex flex-col items-center justify-center mb-5">
+            <BoltIcon className="h-8 w-8" />
+            <h2>Capabilities</h2>
+          </div>
+
+          <div className="space-y-2">
+            <p className="infoText">Change the ChatGPT Model to use</p>
+            <p className="infoText">
+              Messages are stored in Firbase's Firestore
+            </p>
+            <p className="infoText">
+              Hot Toast notification when ChatGPT is thinking!
+            </p>
+          </div>
+        </div>
+
+        {/* Limitations */}
+        <div>
+          <div className="flex flex-col items-center justify-center mb-5">
+            <ExclamationTriangleIcon className="h-8 w-8" />
+            <h2>Limitations</h2>
+          </div>
+
+          <div className="space-y-2">
+            <p className="infoText">
+              May occasionally generate incorrect information
+            </p>
+            <p className="infoText">
+              May occasionally produce harmful instructions or biased content
+            </p>
+            <p className="infoText">
+              Limited knowledge of world events after 2021
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
